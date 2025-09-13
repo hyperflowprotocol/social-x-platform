8eb785c6ee6703cefca6506283aebb7f15493ba0
hyperflowprotocol <bsslp79@gmail.com>
2025-09-13 20:18:08 +0800

    Remove placeholder text from WalletFundTransfer

--- frontend/src/components/WalletFundTransfer.jsx
+++ frontend/src/components/WalletFundTransfer.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 
 const WalletFundTransfer = () => {
-  return (
-    <div className="wallet-fund-transfer">
-      <div className="container">
-        <h2>Fund Transfer</h2>
-        <p>This feature is currently disabled for security reasons.</p>
-        <p>Please use official wallet applications for transfers.</p>
-      </div>
-    </div>
-  );
+  return null;
 };
 
 export default WalletFundTransfer;
