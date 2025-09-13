090886699993e987c6f5afa298808981d1885665
hyperflowprotocol <bsslp79@gmail.com>
2025-09-13 18:52:46 +0800

    Add test_simple.sol

---
+++ test_simple.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleTest {
+    uint256 public value;
+    
+    constructor(uint256 _value) {
+        value = _value;
+    }
+    
+    function setValue(uint256 _value) external {
+        value = _value;
+    }
+}
